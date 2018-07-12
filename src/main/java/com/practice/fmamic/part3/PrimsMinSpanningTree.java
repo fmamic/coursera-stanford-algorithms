65535ff08d1c66651d2fca37682b08c7ee524b51
fmamic <filipmamic@hotmail.com>
2018-07-12 21:20:51 +0100

    Prims Spanning Tree

--- src/main/java/com/practice/fmamic/part3/PrimsMinSpanningTree.java
+++ src/main/java/com/practice/fmamic/part3/PrimsMinSpanningTree.java
@@ -1,19 +1,75 @@
 package com.practice.fmamic.part3;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
+import com.practice.fmamic.data.structure.Edge;
 import com.practice.fmamic.data.structure.Graph;
 import com.practice.fmamic.data.structure.Vertex;
 
 class PrimsMinSpanningTree {
 
-    int calculateMSTCost(final Graph graph) {
+    // O(V * E) without optimization
+    long calculateMSTCost(final Graph graph) {
 
-        Set<Vertex> visited = new HashSet<>();
-        
+        final Set<Vertex> visited = new HashSet<>();
 
-        return 0;
+        visited.add(graph.getVertices().get(0));
+
+        long sum = 0;
+        while (visited.size() != graph.getVertices().size()) {
+
+            Edge eMin = null; Vertex vMin = null;
+            for (final Edge edge : graph.getEdges()) {
+                if (visited.contains(edge.getSource()) && !visited.contains(edge.getDestination()) && (eMin == null || eMin.getWeight() > edge.getWeight())) {
+                    vMin = edge.getDestination();
+                    eMin = edge;
+                } else if (!visited.contains(edge.getSource()) && visited.contains(edge.getDestination()) && (eMin == null || eMin.getWeight() > edge.getWeight())) {
+                    eMin = edge;
+                    vMin = edge.getSource();
+                }
+            }
+
+            if (eMin != null && vMin != null) {
+                visited.add(vMin);
+                sum += eMin.getWeight();
+            }
+
+        }
+
+        return sum;
     }
 
+    // O(V * logE)
+    long calculateMSTCostOptimized(final Graph graph) {
+
+        final Set<Vertex> visited = new HashSet<>();
+
+        visited.add(graph.getVertices().get(0));
+
+        long sum = 0;
+        while (visited.size() != graph.getVertices().size()) {
+
+            Edge eMin = null; Vertex vMin = null;
+            for (final Edge edge : graph.getEdges()) {
+                if (visited.contains(edge.getSource()) && !visited.contains(edge.getDestination()) && (eMin == null || eMin.getWeight() > edge.getWeight())) {
+                    vMin = edge.getDestination();
+                    eMin = edge;
+                } else if (!visited.contains(edge.getSource()) && visited.contains(edge.getDestination()) && (eMin == null || eMin.getWeight() > edge.getWeight())) {
+                    eMin = edge;
+                    vMin = edge.getSource();
+                }
+            }
+
+            if (eMin != null && vMin != null) {
+                visited.add(vMin);
+                sum += eMin.getWeight();
+            }
+
+        }
+
+        return sum;
+    }
 }
--- src/test/java/com/practice/fmamic/part3/Part3Test.java
+++ src/test/java/com/practice/fmamic/part3/Part3Test.java
@@ -77,10 +77,38 @@ public class Part3Test {
     }
 
     @Test
-    public void primsSpanningTreeTest() {
+    public void primsSpanningTreeTest2() {
+        final PrimsMinSpanningTree primsMinSpanningTree = new PrimsMinSpanningTree();
+
+        final Graph graph = new Graph();
+
+        Vertex v1 = new Vertex(1);
+        Vertex v2 = new Vertex(2);
+        Vertex v3 = new Vertex(3);
+        Vertex v4 = new Vertex(4);
+        Vertex v5 = new Vertex(5);
+
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+        graph.addVertex(v5);
+
+        graph.addEdge(v1, v2, 1);
+        graph.addEdge(v2, v3, 2);
+        graph.addEdge(v3, v4, 4);
+        graph.addEdge(v2, v4, 3);
+        graph.addEdge(v3, v5, 5);
+        graph.addEdge(v5, v1, 6);
+
+        assertEquals(11, primsMinSpanningTree.calculateMSTCost(graph));
+    }
+
+    @Test
+    public void primsSpanningTreeTest1() {
         final PrimsMinSpanningTree primsMinSpanningTree = new PrimsMinSpanningTree();
         final Graph graph = getGraphData("MST.txt");
-        assertEquals(11, primsMinSpanningTree.calculateMSTCost(graph));
+        assertEquals(-3612829, primsMinSpanningTree.calculateMSTCost(graph));
     }
 
     private Graph getGraphData(final String s) {
